61e4eb645acd4eab756ef0bf9024f841255edfef
Clare Calandra <ccalandra1@binghamton.edu>
2024-12-05 14:24:29 -0500

    Final submission

--- template_final_project-master/main.py
+++ template_final_project-master/main.py
@@ -1,18 +1,11 @@
 import pygame 
 from src.controller import Controller 
-#import your controller             from src.controller import controller 
 SCREEN_WIDTH = 1000 
 SCREEN_HEIGHT = 550 
-KEY_HEIGHT = 250 
-KEY_WIDTH = 100 
 def main():
     pygame.init() 
     controller = Controller(SCREEN_WIDTH, SCREEN_HEIGHT) 
     controller.mainloop()
-    #Create an instance on your controller object
-    #Call your mainloop
-    
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 
 # https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
